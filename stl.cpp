4db3b16acd9d30fb8ce54a1153cc56298151a430
Saladisowjanya29-git <saladisowjanya45@gmail.com>
2025-08-16 18:01:15 +0530

    Add files via upload

---
+++ stl.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+void print()
+{
+	cout<<"raj";
+}
+void explationopair()
+{
+	pair<int ,int> p = {1,3};
+	cout<<p.first<<" "<<p.second;
+}
+int sum(int a,int b)
+{
+return a+b;
+}
+int main()
+{
+	print();
+int h=sum(1,3);
+cout<<h<<endl;
+cout<<p.first<<" "<<p.second;
+	return 0;
+}
